81a5f0c91195cb0ff6f95c29696b4aeae85cc856
rehankhan21 <narutop224@gmail.com>
2023-01-21 01:07:41 -0500

    added test for button click event using fireEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import { logRoles } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial color', () => {
-  const { container } =  render(<App />);
-  logRoles(container);
-
+test('button has correct initial color, and updates when clicked', () => {
+  render(<App />);
+  
   // find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  // click button 
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent('Change to red');
 });
 
-test('button has correctr initial text', () => {
+test('button has correct initial text', () => {
 
 });
 
-test('button turns blue when clicked', () => {
+// test('button turns blue when clicked', () => {
+//   render(<App />);
 
-});
+//   const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+// });
